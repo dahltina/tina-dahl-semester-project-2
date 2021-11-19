97e8c82bb7238b91200954258ee32f55454a4425
Tina Dahl <tina@e5r.no>
2021-11-19 10:33:36 -0500

    Fix async function that threw an error in Safari

--- js/index.js
+++ js/index.js
@@ -10,7 +10,6 @@ async function getProducts() {
     try {
         const response = await fetch(productsUrl);
         const json = await response.json();
-        console.log(json);
         const products = json;
         createProducts(products, container);
 
@@ -29,23 +28,18 @@ async function getImages() {
     try {
         const response = await fetch(imageUrl);
         const image = await response.json();
-        console.log(image);
-        return image;
+        const hero = document.querySelector(".hero-container");
+        hero.innerHTML += `<img class="banner-img" src="${image.hero_banner.url}" alt="${image.hero_banner.alternativeText}">`;
     }
     catch (error) {
         console.log(error);
     }
 }
 
-//display hero image
-const image = await getImages();
-const hero = document.querySelector(".hero-container");
-
-hero.innerHTML += `<img class="banner-img" src="${image.hero_banner.url}" alt="${image.hero_banner.alternativeText}">`;
+getImages();
 
 
 // get articles
-
 const postsUrl = baseUrl + "posts";
 const postContainer = ".post-container";
 
@@ -53,7 +47,6 @@ async function getPosts() {
     try {
         const response = await fetch(postsUrl);
         const post = await response.json();
-        console.log(post);
         createArticles(post, postContainer);
     }
     catch (error) {
